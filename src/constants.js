d4a80d25460e7cf893c9aba5962a44bfed7d7ac4
pbukala-ct <piotr.bukala@commercetools.com>
2025-03-24 18:36:51 +1100

    constant update

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 // Make sure to import the helper functions from the `ssr` entry point.
 import { entryPointUriPathToPermissionKeys } from '@commercetools-frontend/application-shell/ssr';
 
-export const entryPointUriPath = 'live-dashboard';
+export const entryPointUriPath = 'live-orders';
 
 export const PERMISSIONS = entryPointUriPathToPermissionKeys(entryPointUriPath);
